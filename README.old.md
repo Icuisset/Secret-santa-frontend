13b94401c6fd4c2e9812aa189f8ccc5d96ce6646
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-11-10 16:22:08 +0000

    initial set up ok

---
+++ README.old.md
@@ -0,0 +1,7 @@
+# Collection_Santa_Frontend
+
+FrontEnd React Application for MEET YOUR SANTA mini project
+
+_____________________
+
+- React.js
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="santa">
+     <h1>Merchandising Secret Santa 2021</h1>
+    </div>
+  );
+}
+
+export default App;
