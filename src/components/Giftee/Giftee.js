a6f225831a4efb67b68e565bbe316757885fd70d
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-11-14 17:16:31 +0000

    updated avatar and message

--- src/components/Giftee/Giftee.js
+++ src/components/Giftee/Giftee.js
@@ -5,13 +5,33 @@ import { gsap } from "gsap";
 
 export default function Giftee({ giftee, selectedName }) {
   const nameRef = useRef();
+  const avatarRef = useRef();
+  const messageRef = useRef();
+  const detailRef = useRef();
+  const tl = useRef();
 
   useEffect(() => {
-    gsap.to(nameRef.current, {
-      rotation: "+=360",
-      duration: 1,
-      ease: "elastic",
-    });
+    tl.current = gsap
+      .timeline()
+      .to(nameRef.current, {
+        rotation: 360,
+        duration: 1,
+        ease: "elastic",
+      })
+      .from(avatarRef.current, {
+        opacity: 0,
+        scale: 0,
+        duration: 1,
+      })
+      .from(messageRef.current, {
+        opacity: 0,
+        y: 500,
+      })
+      .from(detailRef.current, {
+        opacity: 0,
+        y: 500,
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -22,6 +42,20 @@ export default function Giftee({ giftee, selectedName }) {
           <p className='giftee-name' ref={nameRef}>
             {giftee.name}
           </p>
+          <img
+            src={giftee.avatar}
+            alt='giftee avatar'
+            width='200'
+            ref={avatarRef}
+          />
+          <p className='giftee-message' ref={messageRef}>
+            And here is a personal message <br />
+            to you:
+          </p>
+          <div ref={detailRef}>
+            <p className='giftee-santa giftee-text'>Dear Santa,</p>
+            <p className='giftee-message giftee-text'>{giftee.message}</p>
+          </div>
         </div>
       ) : null}
     </div>
