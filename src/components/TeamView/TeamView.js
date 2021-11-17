0cf6dc829a152b05a803618ad7277e6fb5554756
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-11-17 13:35:53 +0000

    Update TeamView.js

--- src/components/TeamView/TeamView.js
+++ src/components/TeamView/TeamView.js
@@ -6,9 +6,9 @@ import "./TeamView.css";
 export default function TeamView() {
   return (
     <div className='show-zone'>
-      <p className='show-text'>Everyone is ready for Santa!</p>
+      <p className='show-text'>Ready for Santa!</p>
       <Link to='/team' className='show-button'>
-        View my Team
+        View your Team
       </Link>
       <p className='show-text_between'> - OR - </p>
       <Link to='/draw' className='show-button'>
